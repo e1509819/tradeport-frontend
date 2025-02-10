387db3551ff7e74741d348113720911af9416671
RANJITHNAMBIAR <ranjithxyz@gmail.com>
2025-02-10 08:28:22 +0800

    Fixed Build Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import  { ProductMaster } from './Product';
 import { ThankYouPage } from './ThankYouPage';
+import { savePost } from './posts/savePost';
 
+import { Product } from './posts/types';
+
+async function handleSave(product: Product) {
+  const newPost = await savePost(product);
+  console.log(newPost);
+}
 const router = createBrowserRouter([
   {
     path: '/',
@@ -9,7 +16,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/product',
-    element: <ProductMaster />,
+    element: <ProductMaster onSave={handleSave}/>,
   },
   {
     path: '/thank-you',
