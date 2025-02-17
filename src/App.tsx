154af01915b27b26a84661e11571420cbd007b98
sree <sreeraj_ec@hotmail.com>
2025-02-17 20:09:08 +0800

    docs(contributing): Updated the contributing.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  { ProductMaster } from './Product';
-import { ThankYouPage } from './ThankYouPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductMaster } from "./Product";
+import { ThankYouPage } from "./ThankYouPage";
+
 const App = () => {
-  return(<BrowserRouter> 
-  <Routes>
-    <Route path="/" element={<ProductMaster />} />
-    <Route path="/thank-you" element={<ThankYouPage />} />
-  </Routes>
-  </BrowserRouter> )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductMaster />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
