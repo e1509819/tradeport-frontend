c6713f2297ac9b000c5ac2b84c9154ad5d5d03b9
sree <sreeraj_ec@hotmail.com>
2025-02-17 20:26:14 +0800

    refactor(pages): product and thank you moved into pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ProductMaster } from "./Product";
-import { ThankYouPage } from "./ThankYouPage";
-
+import { ProductMaster } from "./pages/Product";
+import { ThankYouPage } from "./pages/ThankYouPage";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductMaster />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductMaster />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex bg-red-200">
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
