709daefa397cd4482f2c32db1668d30753e714f0
sree <sreeraj_ec@hotmail.com>
2025-02-19 21:03:20 +0800

    fix(products): Updated the paths in the products page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,31 +1,34 @@
-import { useForm } from 'react-hook-form';  
-import { Product, SavedPost } from './posts/types';
-import { useNavigate } from 'react-router-dom';
-import { savePost } from './posts/savePost';
-import { ValidationError } from './posts/ValidationError';
-import { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { Product, SavedPost } from "../posts/types";
+import { useNavigate } from "react-router-dom";
+import { savePost } from "../posts/savePost";
+import { ValidationError } from "../posts/ValidationError";
+import { useState } from "react";
 
 export function ProductMaster() {
-  const { register, handleSubmit, formState: { errors } } = useForm<Product>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const [image, setImage] = useState<File | null>(null);
   const navigate = useNavigate();
 
   const onSubmit = async (product: Product): Promise<void> => {
-    console.log('Submitted details:', product);
+    console.log("Submitted details:", product);
     try {
-   
-      const body: SavedPost = await savePost(product,image);
-      console.log('response:', body);
+      const body: SavedPost = await savePost(product, image);
+      console.log("response:", body);
       navigate(`thank-you`);
     } catch (error) {
-      console.error('Error saving post:', error);
+      console.error("Error saving post:", error);
     }
   };
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?e.target.files[0] : null;
-    
+    const file = e.target.files ? e.target.files[0] : null;
+
     setImage(file);
     if (file) {
       setImagePreview(URL.createObjectURL(file));
@@ -35,36 +38,50 @@ export function ProductMaster() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-blue-400 overflow-auto">
-      <div className="bg-white p-6 md:p-20 rounded-lg shadow-2xl w-full max-w-lg">
+    <div className="flex min-h-screen items-center justify-center overflow-auto bg-blue-400">
+      <div className="w-full max-w-lg rounded-lg bg-white p-6 shadow-2xl md:p-20">
         <div className="flex flex-col justify-center space-y-5">
-          <h2 className="font-serif text-xl font-medium text-center text-black">Product Master</h2>
-          <p className="font-serif text-black-500 text-center">Add New Products</p>
-          <form noValidate onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+          <h2 className="text-center font-serif text-xl font-medium text-black">
+            Product Master
+          </h2>
+          <p className="text-black-500 text-center font-serif">
+            Add New Products
+          </p>
+          <form
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            className="space-y-4"
+          >
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <input
                 type="text"
                 id="productName"
                 placeholder="Product Name"
-                className="p-2 px-4 text-center border-2 border-gray-300 rounded-lg placeholder:text-xs md:placeholder:text-sm"
-                {...register('productName', { required: 'You must enter Product Name' })}
+                className="rounded-lg border-2 border-gray-300 p-2 px-4 text-center placeholder:text-xs md:placeholder:text-sm"
+                {...register("productName", {
+                  required: "You must enter Product Name",
+                })}
               />
             </div>
             <ValidationError fieldError={errors.productName} />
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <textarea
                 id="description"
-                className="p-2 border-2 border-gray-300 rounded-lg text-sm"
+                className="rounded-lg border-2 border-gray-300 p-2 text-sm"
                 placeholder="Description"
-                {...register('description', { required: 'You must enter Description' })}
+                {...register("description", {
+                  required: "You must enter Description",
+                })}
               ></textarea>
             </div>
             <ValidationError fieldError={errors.description} />
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <select
                 id="category"
-                className="p-2 border-2 border-gray-300 rounded-lg"
-                {...register('category', { required: 'You must select a category' })}
+                className="rounded-lg border-2 border-gray-300 p-2"
+                {...register("category", {
+                  required: "You must select a category",
+                })}
               >
                 <option value="">Select Category</option>
                 <option value="101">Furniture</option>
@@ -75,33 +92,41 @@ export function ProductMaster() {
               </select>
             </div>
             <ValidationError fieldError={errors.category} />
-            <div className="flex flex-row gap-x-2 md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-row gap-x-2 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <input
                 type="file"
                 id="image"
-                className="p-2 border-2 border-gray-300 rounded-lg"
+                className="rounded-lg border-2 border-gray-300 p-2"
                 accept="image/*"
                 onChange={handleImageChange}
               />
             </div>
             {imagePreview && (
               <div className="flex justify-center">
-                <img src={imagePreview} alt="Product Preview" className="max-w-full h-auto mt-4" />
+                <img
+                  src={imagePreview}
+                  alt="Product Preview"
+                  className="mt-4 h-auto max-w-full"
+                />
               </div>
             )}
             <ValidationError fieldError={errors.productimage} />
-            <div className="flex flex-row gap-x-2 md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-row gap-x-2 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <input
                 id="retailPrice"
                 type="number"
-                className="p-2 border-2 border-gray-300 rounded-lg"
+                className="rounded-lg border-2 border-gray-300 p-2"
                 placeholder="Retail Price"
-                {...register('retailPrice', { required: 'You must enter Retail Price' })}
+                {...register("retailPrice", {
+                  required: "You must enter Retail Price",
+                })}
               />
               <select
                 id="retailcurrency"
-                className="p-2 border-2 border-gray-300 rounded-lg"
-                {...register('retailCurrency', { required: 'You must select a currency' })}
+                className="rounded-lg border-2 border-gray-300 p-2"
+                {...register("retailCurrency", {
+                  required: "You must select a currency",
+                })}
               >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
@@ -111,18 +136,22 @@ export function ProductMaster() {
             </div>
             <ValidationError fieldError={errors.retailPrice} />
             <ValidationError fieldError={errors.retailCurrency} />
-            <div className="flex flex-row gap-x-2 md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-row gap-x-2 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <input
                 type="number"
                 id="wholesaleprice"
-                className="p-2 border-2 border-gray-300 rounded-lg"
+                className="rounded-lg border-2 border-gray-300 p-2"
                 placeholder="Wholesale Price"
-                {...register('wholesalePrice', { required: 'You must enter Wholesale Price' })}
+                {...register("wholesalePrice", {
+                  required: "You must enter Wholesale Price",
+                })}
               />
               <select
                 id="wholesalecurrency"
-                className="p-2 border-2 border-gray-300 rounded-lg"
-                {...register('wholeSaleCurrency', { required: 'You must select a currency' })}
+                className="rounded-lg border-2 border-gray-300 p-2"
+                {...register("wholeSaleCurrency", {
+                  required: "You must select a currency",
+                })}
               >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
@@ -132,18 +161,21 @@ export function ProductMaster() {
             </div>
             <ValidationError fieldError={errors.wholeSaleCurrency} />
             <ValidationError fieldError={errors.wholesalePrice} />
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
               <input
                 type="number"
                 id="stock"
-                className="p-2 border-2 border-gray-300 rounded-lg"
+                className="rounded-lg border-2 border-gray-300 p-2"
                 placeholder="Stock"
-                {...register('quantity', { required: 'You must enter Stock' })}
+                {...register("quantity", { required: "You must enter Stock" })}
               />
             </div>
             <ValidationError fieldError={errors.quantity} />
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
-              <button type="submit" className="px-5 py-3 text-xs md:text-sm bg-blue-500 text-white border-gray-300 rounded-md">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+              <button
+                type="submit"
+                className="rounded-md border-gray-300 bg-blue-500 px-5 py-3 text-xs text-white md:text-sm"
+              >
                 Create Product
               </button>
             </div>
@@ -153,4 +185,3 @@ export function ProductMaster() {
     </div>
   );
 }
-
